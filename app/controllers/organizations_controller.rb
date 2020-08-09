429c1c9ff72c7722e755f797f71b688d5464ce3e
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-08-09 20:11:40 +0530

    Organization controller

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,22 @@
+class OrganizationsController < ApplicationController
+  before_action :set_organization, only: [:show, :edit, :update, :destroy]
+  layout 'dashboard'
+
+  # GET /organizations
+  # GET /organizations.json
+  def index
+    @organizations = Organization.all
+  end
+
+  # GET /organizations/1
+  # GET /organizations/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
+
+end
