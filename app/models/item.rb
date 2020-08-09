5c7ee17dbb240c3ee3c1468e3877437c1643435f
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-08-09 13:48:32 +0530

    Item and organization validations/associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :organization
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :content, presence: true
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,21 @@
 class Organization < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :display_image, presence: true
+
+  
+  has_many :items, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :places, dependent: :destroy
+
+  enum kind: { gallery: 1, museum: 2, library: 3}
+
+  def latitude
+    location.split(",")[0] || ''
+  end
+
+  def longitude
+    location.split(",")[1] || ''
+  end
 end
