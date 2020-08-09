c4816facc9f2c7d86e1e9b9832f448572414bc92
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-08-09 13:11:52 +0530

    Events validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  belongs_to :organization
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :starts_at, presence: true
+  validates :duration, presence: true
 end
