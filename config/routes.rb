20268fcdd7ce9fe3d883e29d7777980383f2deb9
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-08-09 20:28:47 +0530

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/items'
-  get 'pages/events'
-  get 'pages/places'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :organizations
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'explore', to: 'organizations#index'
+  get '3d', to: 'pages#items'
+  get 'events', to: 'pages#events'
+  get 'places', to: 'pages#places'
+
+  root to: 'pages#home'
 end
