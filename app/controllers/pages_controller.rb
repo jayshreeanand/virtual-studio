26234511c638be68e846ac8a59e93e3efe3a3b2d
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-08-10 06:45:53 +0530

    Dashboard fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
+  layout 'dashboard', only: [:items, :events, :places]
   def home
   end
 
   def items
+    @items = Item.all
   end
 
   def events
+    @events = Event.all
   end
 
   def places
+    @places = Place.all
   end
 end
